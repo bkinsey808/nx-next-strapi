d750ea226b68b683350cad90f854c64e3f42c22e
Ben Kinsey <ben.kinsey@carrier.com>
2021-10-09 07:06:18 -1000

    improve accessibility lighthouse score

---
+++ apps/client/pages/_document.tsx
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+// see https://nextjs.org/docs/advanced-features/custom-document
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta
+            name="description"
+            content="A page to demonstrate NextJs and Urql working together"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
